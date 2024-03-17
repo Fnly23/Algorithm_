c70914af529160ba3df03613dda9e54527f31dbb
Fnly23 <145765813+Fnly23@users.noreply.github.com>
2024-03-17 23:44:18 +0000

    Add files via upload

---
+++ Algorithms_And_Complexity/AlgorithmsAndComplexity/AlgorithmsAndComplexity/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class fileReader
+{
+    private static int[] numbers;
+
+
+    public static void ReadFile(string filename)
+    {
+        bool exit = false;
+        while (!exit)
+        {
+            string directory = Directory.GetCurrentDirectory();
+
+            string filepath = Path.Combine(directory, filename);
+
+            string[] lines = File.ReadAllLines(filepath);
+            numbers = new int[lines.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (int.TryParse(lines[i], out int num))
+
+                {
+                    numbers[i] = num;
+
+                }
+
+            }
+
+        }
+
+
+
+
+
+    }
+
+}
+
---
+++ Algorithms_And_Complexity/AlgorithmsAndComplexity/AlgorithmsAndComplexity/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+namespace AlgorithmsAndComplexity
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            bool exit = false;
+            
+            
+            while (exit == false) 
+            {
+
+            Console.WriteLine("Which net file to open? [0/1/2/3]");
+            string userInput = Console.ReadLine();
+            Convert.ToInt32(userInput);
+                if (userInput == "1") 
+                {
+                    fileReader.ReadFile("Net_1_256.txt");
+
+                    Console.WriteLine("Net_1_256.txt");
+                    Console.ReadKey();  
+                
+                }
+
+
+
+
+            }
+
+
+
+        }
+    }
+}
